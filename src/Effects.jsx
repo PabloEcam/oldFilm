959a3865e347fa02135141ec1037c5246e1e7c4e
Pablo Fernando Mesquida <pablo.mesquida@ecam.es>
2023-10-04 11:18:47 +0200

    leva

--- src/Effects.jsx
+++ src/Effects.jsx
@@ -1,33 +1,131 @@
-
-import React, { useState } from 'react';
+import { useState } from 'react'
 import { useFrame } from '@react-three/fiber'
-import { EffectComposer, Noise, Vignette, Texture } from '@react-three/postprocessing'
+import {
+  EffectComposer,
+  Noise,
+  Vignette,
+  Texture
+} from '@react-three/postprocessing'
 import { BlendFunction } from 'postprocessing'
+import { useControls, folder } from 'leva'
 
-function Effects(){
-    const [currentTexture, setCurrentTexture] = useState("./camera01.jpg");
-    const [frameCount, setFrameCount] = useState(0);
+function Effects() {
+  const [currentTexture, setCurrentTexture] = useState(
+    './texture1/camera01.jpg'
+  )
+  const [frameCount, setFrameCount] = useState(0)
+  const [frameInterval, setFrameInterval] = useState(5)
+  const [internalCounter, setInternalCounter] = useState(0)
 
-    const textureList = [
-        "./camera01.jpg",
-        "./camera03.jpg",
-        "./camera04.jpg",
-        "./camera05.jpg",
-        "./camera06.jpg",
-    ];
-    
-    useFrame(() => {
-        setCurrentTexture(textureList[frameCount]);
-        setFrameCount((prevContador) => (prevContador + 1) % textureList.length);
-    });
-    
-    return(
-        <EffectComposer > 
-            <Noise opacity={0.1} />
-            <Vignette eskil={false} offset={0.1} darkness={0.9} />
-            <Texture blendFunction={BlendFunction.ADD} textureSrc={currentTexture}/>
-        </EffectComposer>
-    )
+  const textureList1 = [
+    './texture1/camera01.jpg',
+    './texture1/camera03.jpg',
+    './texture1/camera04.jpg',
+    './texture1/camera05.jpg',
+    './texture1/camera06.jpg'
+  ]
+
+  const textureList2 = Array.from(
+    { length: 40 },
+    (_, i) =>
+      `./texture2/Old_Film_Flashes_Scratches_Burns_Film_Grain_Projector_Hash_03-${String(
+        i
+      ).padStart(4, '0')}.jpg`
+  )
+
+  const {
+    texture,
+    blend,
+    opacityNoise,
+    multiplyNoise,
+    darknessVignette,
+    offsetVignette
+  } = useControls({
+    Texure: folder({
+      texture: {
+        options: { OldFilm1: textureList1, OldFilm2: textureList2 },
+        value: textureList1
+      },
+      blend: {
+        options: {
+          ADD: BlendFunction.ADD,
+          ALPHA: BlendFunction.ALPHA,
+          AVERAGE: BlendFunction.AVERAGE,
+          COLOR: BlendFunction.COLOR,
+          COLOR_BURN: BlendFunction.COLOR_BURN,
+          COLOR_DODGE: BlendFunction.COLOR_DODGE,
+          DARKEN: BlendFunction.DARKEN,
+          /*  DIFFERENCE: BlendFunction.DIFFERENCE, */
+          DIVIDE: BlendFunction.DIVIDE,
+          DST: BlendFunction.DST,
+          /* EXCLUSION: BlendFunction.EXCLUSION, */
+          HARD_LIGHT: BlendFunction.HARD_LIGHT,
+          HARD_MIX: BlendFunction.HARD_MIX,
+          HUE: BlendFunction.HUE,
+          /* INVERT: BlendFunction.INVERT, */
+          /* INVERT_RGB: BlendFunction.INVERT_RGB, */
+          LIGHTEN: BlendFunction.LIGHTEN,
+          LINEAR_BURN: BlendFunction.LINEAR_BURN,
+          LINEAR_DODGE: BlendFunction.LINEAR_DODGE,
+          LINEAR_LIGHT: BlendFunction.LINEAR_LIGHT,
+          LUMINOSITY: BlendFunction.LUMINOSITY,
+          MULTIPLY: BlendFunction.MULTIPLY,
+          /*  NEGATION: BlendFunction.NEGATION, */
+          NORMAL: BlendFunction.NORMAL,
+          OVERLAY: BlendFunction.OVERLAY,
+          PIN_LIGHT: BlendFunction.PIN_LIGHT,
+          REFLECT: BlendFunction.REFLECT,
+          SCREEN: BlendFunction.SCREEN,
+          SRC: BlendFunction.SRC,
+          SATURATION: BlendFunction.SATURATION,
+          SOFT_LIGHT: BlendFunction.SOFT_LIGHT,
+          VIVID_LIGHT: BlendFunction.VIVID_LIGHT
+        },
+        value: BlendFunction.ADD
+      }
+    }),
+    Noise: folder({
+      opacityNoise: {
+        value: 0.05,
+        min: 0,
+        max: 1
+      },
+      multiplyNoise: { value: true }
+    }),
+    Vignette: folder({
+      darknessVignette: {
+        value: 1,
+        min: 0,
+        max: 5
+      },
+      offsetVignette: { value: 0.1, min: -5, max: 5 }
+    })
+  })
+
+  useFrame(() => {
+    setInternalCounter((prevCounter) => (prevCounter + 1) % frameInterval)
+
+    if (internalCounter === 0) {
+      if (frameCount < texture.length) {
+        setCurrentTexture(texture[frameCount])
+        setFrameCount((prevContador) => (prevContador + 1) % texture.length)
+      } else {
+        setFrameCount(0)
+      }
+    }
+  })
+
+  return (
+    <EffectComposer>
+      <Noise opacity={opacityNoise} premultiply={multiplyNoise} />
+      <Vignette
+        eskil={false}
+        offset={offsetVignette}
+        darkness={darknessVignette}
+      />
+      <Texture blendFunction={blend} textureSrc={currentTexture} />
+    </EffectComposer>
+  )
 }
 
-export default Effects;
+export default Effects
