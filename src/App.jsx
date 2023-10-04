2f098c38e1e64ed6d0294ceb13fd85c9c38a43e2
Pablo Fernando Mesquida <pablo.mesquida@ecam.es>
2023-10-04 11:26:43 +0200

    r3f-perf

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { Canvas } from '@react-three/fiber'
 import { Environment, OrbitControls } from '@react-three/drei'
 import Effects from './Effects'
-import Cycle from "./Cycle_Glb"
+import Cycle from './Cycle_Glb'
+import { Perf } from 'r3f-perf'
 
 function App() {
   return (
-   <Canvas>
+    <Canvas>
+      <Perf position='top-left' />
       <OrbitControls />
       <Environment background preset='city' />
       <Effects />
-      <Cycle position={[0,-1,0]} />
-   </Canvas>
+      <Cycle position={[0, -1, 0]} />
+    </Canvas>
   )
 }
 
